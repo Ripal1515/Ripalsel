587d2779d1ce7c7e018228527624a9e9ed851931
Ripal1515 <joshi.ripal@gmail.com>
2022-03-17 14:29:14 -0500

    My sel project

---
+++ src/test/java/com/sel/test/Login.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.sel.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author srmhome
+ */
+public class Login {
+
+    public Login() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testLogin() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://www.advantageonlineshopping.com/#/");
+        driver.findElement(By.id("menuUser")).click();
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("username")).sendKeys("Johan");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("Home!@#$%1");
+        driver.findElement(By.id("sign_in_btnundefined")).click();
+        driver.findElement(By.id("menuCart")).click();
+        driver.findElement(By.id("checkOutButton")).click();
+        driver.findElement(By.id("next_btn")).click();
+    }
+
+}
